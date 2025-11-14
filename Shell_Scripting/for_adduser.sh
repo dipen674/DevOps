97fdfc5ead50d2e1fb4d05df014847720c08b591
Deependra <bhattadeependra05@gmail.com>
2025-11-14 21:57:02 +0545

    Added docker files and removed scripts and vms files

--- Shell_Scripting/for_adduser.sh
+++
@@ -1,9 +0,0 @@
-for x in ram shyam hari
-do
-	echo "Adding user $x"
-	sudo useradd $x
-	id $x
-	sleep 4s
-done 
-
-
--- Shell_Scripting/while.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-counter=0
-while [ $counter -lt 5 ]
-do
-	echo "The value of counter is $counter"
-	counter=(`counter+1`)
-	sleep 2s
-done
-echo "We are outside the while loop"
